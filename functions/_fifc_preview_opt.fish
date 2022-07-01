398dbe43bf6d59ac2c284ec7cea98368f8ead86d
gazorby <gazorby@pm.me>
2022-07-01 12:58:51 +0200

    🐛 fix(preview opt): fallback to fish desc if there is no man page

--- functions/_fifc_preview_opt.fish
+++ functions/_fifc_preview_opt.fish
@@ -17,6 +17,12 @@ function _fifc_preview_opt -d "Open man page of a command starting at the select
     else
         set out (man $cmd 2>/dev/null | string trim | pcregrep --multiline $regex_pcre)
     end
+
+    # Fallback to fish description if there is no man page
+    if test -z "$out"
+        echo "$desc"
+    end
+
     # Pretty printing
     set_color brgreen; echo -e "$out[1]"
     set_color --bold white; echo -e (string join -- "\n" "" $out[2..-1])
