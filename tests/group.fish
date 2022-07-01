12e1138d39d7617c76f53b7152f78bae398ab168
gazorby <gazorby@pm.me>
2022-07-01 20:04:52 +0200

    ✅ test(group): add options group test

--- tests/group.fish
+++ tests/group.fish
@@ -11,5 +11,11 @@ set _fifc_commandline "$_commandline"
 set actual (_fifc_completion_group)
 @test "group test files" "$actual" = files
 
+set _commandline "ls --"
+set _fifc_complist (complete -C --escape -- "$_commandline")
+set _fifc_commandline "$_commandline"
+set actual (_fifc_completion_group)
+@test "group test options" "$actual" = options
+
 set -e _fifc_complist
 set -e _fifc_commandline
