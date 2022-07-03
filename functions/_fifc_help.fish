f632264b63895da9f2acc9513f65c160b38c2091
gazorby <gazorby@pm.me>
2022-07-03 22:53:45 +0200

    📝 docs: update help

--- functions/_fifc_help.fish
+++ functions/_fifc_help.fish
@@ -1,7 +1,10 @@
 function __fifc_help_print
-    argparse -s "i/indentation=?" "l/level=?" e/escape no-color -- $argv
+    argparse -s "i/indentation=?" "l/level=?" "c/color=?" e/escape n -- $argv
     set -l spaces "$_flag_i"
     set -l level "$_flag_l"
+    set -l color "$_flag_c"
+    set -l echo_opt
+
     if test -z "$spaces"
         set spaces 4
     end
@@ -9,40 +12,42 @@ function __fifc_help_print
         set level 1
     end
     if test -n "$_flag_e"
-        set echo_opt -e
+        set -a echo_opt -e
+    end
+    if test -n "$_flag_n"
+        set -a echo_opt -n
+    end
+    if test -z "$_flag_c"
+        set color -o white
     end
     set indent (string repeat --count (math $spaces x $level) " ")
-    set out (string join -- "\n$indent" $argv)
-    if test -n "$_flag_no_color"
-        echo $echo_opt "$indent$out"
-    else
-        set_color -o white
-        echo $echo_opt "$indent$out"
-        set_color normal
+
+    set_color $color
+    echo $echo_opt "$indent$argv[1]"
+    for line in $argv[2..-1]
+        set_color $color
+        echo $echo_opt "\n$indent$line"
     end
+    # end
+    set_color normal
 end
 
 function __fifc_help_section
-    set_color yellow
-    __fifc_help_print -e -l0 --no-color (string join -- "" (string upper -- "$argv") "\n")
-    set_color normal
+    __fifc_help_print -e -l0 --color=yellow (string join -- "" (string upper -- "$argv") "\n")
 end
 
 function __fifc_help_opt
     set -l opt (string split -- '=' $argv[1])
-    set_color green
-    echo -en (__fifc_help_print --no-color -l1 -- "$opt[1]")
-    set_color yellow
+    __fifc_help_print -n -e --color=green -l1 -- "$opt[1]"
     if test (count $opt) -eq 2
+        set_color yellow
         echo "=$opt[2]"
     else
         echo ""
     end
-    set_color -o white
     set -l desc (string split -- '\n' $argv[2..-1] | string trim)
-    echo -e (__fifc_help_print --no-color -l2 -- $desc)
+    __fifc_help_print -e -l2 -- $desc
     echo ""
-    set_color normal
 end
 
 function _fifc_help -d "Print fifc help message"
@@ -53,6 +58,26 @@ function _fifc_help -d "Print fifc help message"
     __fifc_help_print -e "fifc [OPTIONS]\n"
 
     __fifc_help_section DESCRIPTION
+
+    __fifc_help_print -e -n \
+        "The fifc command allows you to add custom completion rules that can enhance fish completions or override them.\n" \
+        "A rule is composed of condition(s) that, if valid, trigger commands that can:" \
+        "  - Change fzf preview (-p)" \
+        "  - Feed fzf input (-s)" \
+        "  - Execute when fifc_open_keybinding is pressed (defaults to ctrl-o) (-o)"
+
+    __fifc_help_print -e "\n\n"
+
+    __fifc_help_print -e -n \
+        "A condition can be either:" \
+        " - A regex that must match commandline before cursor position (-r)" \
+        " - An arbitrary command that must exit with a non-zero status (-n)"
+
+    __fifc_help_print -e -n \
+        "Rule are evaluated in the order in which they are defined,"
+        "and fifc will stop at the first rule where all conditions are met" \
+
+    __fifc_help_print -e "\n\n"
 
     __fifc_help_opt \
         "-r, --regex=REGEX" \
@@ -83,7 +108,6 @@ function _fifc_help -d "Print fifc help message"
         "The order in which the rule is evaluated." \
         "If missing, the rule will be evaluated after all ordered ones, and all unordered rules defined before."
 
-
     __fifc_help_opt \
         "-f, --fzf-options" \
         "Custom fzf options (can override previous ones)"
@@ -93,27 +117,25 @@ function _fifc_help -d "Print fifc help message"
         "Show this help"
 
     __fifc_help_print -e "Examples:\n"
+
     __fifc_help_print -e -l2 -- "- Preview files using bat (already builtin):\n"
-    set_color white
-    __fifc_help_print --no-color -e -l2 -- \
+    __fifc_help_print -e -l2 --color=white -- \
         '  fifc -n \'test -f "$fifc_candidate"\' -p "bat --color=always $fifc_candidate"'
 
-    __fifc_help_print -e "\n\n"
+    __fifc_help_print -e "\n"
 
     __fifc_help_print -e -l2 -- "- Use fd to search files recursively (already builtin):\n"
-    set_color white
-    __fifc_help_print --no-color -e -l2 -- \
+    __fifc_help_print -e -l2 --color=white -- \
         '  fifc -n \'test "$fifc_group" = files\' -s \'fd . --color=always --strip-cwd-prefix\''
 
-    __fifc_help_print -e "\n\n"
+    __fifc_help_print -e "\n"
 
     __fifc_help_print -e -l2 -- "- Interactively search packages on archlinux:\n"
-    set_color white
-    __fifc_help_print --no-color -e -l2 -- \
-        "  fifc \\ \n" \
-        "  -r '^pacman(\h*\-S)?\h+\w+' \\ \n" \
-        "  -s 'pacman --color=always -Ss "\$fifc_token" | string match -r \"^[^\h+].*\"' \\ \n" \
-        "  -e '.*/(.*?)\h.*' \\ \n" \
-        "  -f '--query \"\"' \\ \n" \
-        "  -p 'pacman -Si "\$fifc_extracted"' \\"
+    __fifc_help_print -e -n -l2 --color=white -- \
+        " fifc \\" \
+        " -r '^pacman(\h*\-S)?\h+\w+' \\" \
+        " -s 'pacman --color=always -Ss "\$fifc_token" | string match -r \"^[^\h+].*\"' \\" \
+        " -e '.*/(.*?)\h.*' \\" \
+        " -f '--query \"\"' \\" \
+        " -p 'pacman -Si "\$fifc_extracted"' \\ \n"
 end
