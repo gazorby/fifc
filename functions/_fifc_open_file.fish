f3947a0280f373f1a2d4c44e7746a9f21d3a30c0
gazorby <gazorby@pm.me>
2022-07-03 18:07:27 +0200

    🐛 fix: fn open

--- functions/_fifc_open_file.fish
+++ functions/_fifc_open_file.fish
@@ -1,20 +1,26 @@
 function _fifc_open_file -d "Open a file with the right tool depending on its type"
-    set -l file_type (_fifc_file_type "$fifc_candidate")
+    set -l filepath "$fifc_candidate"
+
+    if test -n "$argv"
+        set filepath "$argv"
+    end
+
+    set -l file_type (_fifc_file_type "$filepath")
 
     switch $file_type
         case txt json archive
-            $fifc_editor "$fifc_candidate"
+            $fifc_editor "$filepath"
         case image
             if type -q chafa
-                chafa --watch $fifc_chafa_opts "$fifc_candidate"
+                chafa --watch $fifc_chafa_opts "$filepath"
             else
-                $fifc_editor "$fifc_candidate"
+                $fifc_editor "$filepath"
             end
         case binary
             if type -q hexyl
-                hexyl $fifc_hexyl_opts "$fifc_candidate" | less --RAW-CONTROL-CHARS
+                hexyl $fifc_hexyl_opts "$filepath" | less --RAW-CONTROL-CHARS
             else
-                $fifc_editor "$fifc_candidate"
+                $fifc_editor "$filepath"
             end
     end
 end
