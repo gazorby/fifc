87c965fe42a5b2ddb6e0ea84871ca56651db1cb2
gazorby <gazorby@pm.me>
2022-07-04 11:55:42 +0200

    🐛 fix(preview processes): fix error message

--- functions/_fifc_preview_process.fish
+++ functions/_fifc_preview_process.fish
@@ -4,14 +4,12 @@ function _fifc_preview_process -d "Preview process informations"
 
     if type -q procs
         procs --color=always --tree $fifc_procs_opts "$pid"
-        if not ps -p "$pid" &>/dev/null
-            set_color yellow
-            echo -e "$err_msg"
-        end
     else
         set -l ps_preview_fmt (string join ',' 'pid' 'ppid=PARENT' 'user' '%cpu' 'rss=RSS_IN_KB' 'start=START_TIME' 'command')
         ps -o "$ps_preview_fmt" -p "$pid" 2>/dev/null
-        or set_color yellow
+    end
+    if not ps -p "$pid" &>/dev/null
+        set_color yellow
         echo -e "$err_msg"
     end
 end
