409d20ae1a2990ecbc76e977341cf252771f4dd9
gazorby <gazorby@pm.me>
2022-07-08 12:00:25 +0200

    🐛 fix: missing --

--- functions/_fifc_path_to_complete.fish
+++ functions/_fifc_path_to_complete.fish
@@ -1,5 +1,5 @@
 function _fifc_path_to_complete
-    set -l token (string unescape $fifc_token)
+    set -l token (string unescape -- $fifc_token)
     if string match --regex --quiet -- '.*(\w|\.|/)+$' "$token"
         _fifc_expand_tilde "$token"
     else
