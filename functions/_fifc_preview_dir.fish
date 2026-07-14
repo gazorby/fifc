220dbdc15352f67d6ad0bdf2175a280e186ca047
Matthieu <10926130+gazorby@users.noreply.github.com>
2026-07-14 17:13:25 +0200

    ref: replace exa with eza (#70)

--- functions/_fifc_preview_dir.fish
+++ functions/_fifc_preview_dir.fish
@@ -1,5 +1,7 @@
 function _fifc_preview_dir -d "List content of the selected directory"
-    if type -q exa
+    if type -q eza
+        eza --color=always $fifc_eza_opts $fifc_candidate
+    else if type -q exa
         exa --color=always $fifc_exa_opts $fifc_candidate
     else
         ls --color=always $fifc_ls_opts $fifc_candidate
