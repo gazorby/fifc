31b0217da6e884a72802d22bd83c34f6fd46be91
justbispo <38961424+justbispo@users.noreply.github.com>
2026-07-14 17:16:48 +0100

    Fix fzf showing full path suggestions when the current dir has spaces
    Whenever the current directory has spaces, fifc would show the full path instead of simply the file or directory name.

--- functions/_fifc_source_files.fish
+++ functions/_fifc_source_files.fish
@@ -1,6 +1,7 @@
 function _fifc_source_files -d "Return a command to recursively find files"
-    set -l path (_fifc_path_to_complete | string escape)
-    set -l hidden (string match "*." "$path")
+    set -l raw_path (_fifc_path_to_complete)
+    set -l escaped_path (string escape -- $raw_path)
+    set -l hidden (string match "*." "$raw_path")
 
     if string match --quiet -- '~*' "$fifc_query"
         set -e fifc_query
@@ -11,20 +12,20 @@ function _fifc_source_files -d "Return a command to recursively find files"
             set fd_custom_opts --strip-cwd-prefix
         end
 
-        if test "$path" = {$PWD}/
+        if test "$raw_path" = "$PWD/"
             echo "fd . $fifc_fd_opts --color=always $fd_custom_opts"
-        else if test "$path" = "."
+        else if test "$raw_path" = "."
             echo "fd . $fifc_fd_opts --color=always --hidden $fd_custom_opts"
         else if test -n "$hidden"
-            echo "fd . $fifc_fd_opts --color=always --hidden -- $path"
+            echo "fd . $fifc_fd_opts --color=always --hidden -- $escaped_path"
         else
-            echo "fd . $fifc_fd_opts --color=always -- $path"
+            echo "fd . $fifc_fd_opts --color=always -- $escaped_path"
         end
     else if test -n "$hidden"
         # Use sed to strip cwd prefix
-        echo "find . $path $fifc_find_opts ! -path . -print 2>/dev/null | sed 's|^\./||'"
+        echo "find . $escaped_path $fifc_find_opts ! -path . -print 2>/dev/null | sed 's|^\./||'"
     else
         # Exclude hidden directories
-        echo "find . $path $fifc_find_opts ! -path . ! -path '*/.*' -print 2>/dev/null | sed 's|^\./||'"
+        echo "find . $escaped_path $fifc_find_opts ! -path . ! -path '*/.*' -print 2>/dev/null | sed 's|^\./||'"
     end
 end
