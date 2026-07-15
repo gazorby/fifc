b37cacdf407d849bc3d14a7defdef400beba7c4a
gazorby <gazorby@pm.me>
2026-07-16 00:01:06 +0200

    fix: restore fzf options set by rules

--- functions/_fifc.fish
+++ functions/_fifc.fish
@@ -2,6 +2,7 @@ function _fifc
     set -f --export SHELL (command --search fish)
     set -l result
     set -Ux _fifc_extract_regex
+    set -Ux _fifc_rules_fzf_opts
     set -gx _fifc_complist_path (string join '' (mktemp) "_fifc")
     set -gx fifc_extracted
     set -gx fifc_commandline
@@ -42,7 +43,8 @@ function _fifc
             --bind='$fifc_open_keybinding:execute(_fifc_action open {} {q} &> /dev/tty)' \
             --bind='tab:down,shift-tab:up,ctrl-space:toggle+down' \
             --query '$fifc_safe_query' \
-            $fifc_custom_fzf_opts"
+            $fifc_custom_fzf_opts \
+            $_fifc_rules_fzf_opts"
 
     set -l cmd (string join -- " | " $source_cmd $fzf_cmd)
     # We use eval hack because wrapping source command
@@ -82,6 +84,7 @@ function _fifc
     # Clean state
     command $fifc_rm_cmd $_fifc_complist_path
     set -e _fifc_extract_regex
+    set -e _fifc_rules_fzf_opts
     set -e _fifc_complist_path
     set -e fifc_token
     set -e fifc_group
